f9b72fc1bb76a1892d9ff07414583b793add1a69
ANA SAMOILA <ASA18330@open-groupe.com>
2021-03-18 13:15:23 +0200

    modif

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import settings from './dev-tools-settings-reducer';
-
-const devToolsReducer = combineReducers({
-    settings: settings
-});
-
-export default devToolsReducer;
